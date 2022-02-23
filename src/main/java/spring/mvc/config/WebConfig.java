18918ffbfb11531aba775ac3eb9930b0605343ae
Lamina Samuel <samuellamina2@gmail.com>
2022-02-23 11:05:17 +0100

    Initial commit

---
+++ src/main/java/spring/mvc/config/WebConfig.java
@@ -0,0 +1,11 @@
+package spring.mvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "spring.mvc")
+public class WebConfig {
+}
---
+++ src/main/java/spring/mvc/controller/DemoController.java
@@ -0,0 +1,17 @@
+package spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello bae";
+    }
+}
+
+///Users/samuellamina/IdeaProjects/todolist
